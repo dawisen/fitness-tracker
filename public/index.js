ba2196f1eaa75fece4546980a0592ac4cc92b436
gudetama <daniellewwise@gmail.com>
2021-01-08 20:29:50 -0500

    deploying

---
+++ public/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+init();
+
+async function init() {
+  if (location.search.split("=")[1] === undefined) {
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
+  }
+}
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
