5756f99db34db32f9c76b3e64aa8d364893433d2
gudetama <daniellewwise@gmail.com>
2021-01-11 19:12:25 -0500

    updating path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.get("/all", (req, res) => {
