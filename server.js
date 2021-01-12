9439f1adeb4634a823e28682d717647202259995
gudetama <daniellewwise@gmail.com>
2021-01-11 21:20:15 -0500

    updating css file path

--- server.js
+++ server.js
@@ -5,42 +5,14 @@ const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// use Morgan to monitor HTTP requests made to our server
+//use Morgan to monitor HTTP requests made to our server
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
+app.use(express.static("client"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/all", (req, res) => {
-  db.animals.find({}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/name", (req, res) => {
-  db.animals.find().sort({ name: 1 }, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/weight", (req, res) => {
-  db.animals.find().sort({ weight: -1 }, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
-});
+require("/routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
